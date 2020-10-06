879cab6af00978c6eb221d66efb77269d2ceb4ca
Daniyal Ahmad <57293258+Daniyal-Ahmad358@users.noreply.github.com>
2020-10-06 16:42:57 +0500

    Difference between "For In" and "For Each" loop added

--- Course/Intermediate/3_for_each_loop.dart
+++ Course/Intermediate/3_for_each_loop.dart
@@ -1,1 +1,13 @@
+void main() {
+  var array_a = [1, 2, 3];
 
+  // Syntax 1:
+  array_a.forEach((item) => print('hi $item'));
+
+  // Syntax 2:
+  array_a.forEach((element) {
+    print('hi $element');
+  });
+
+  // How it is different from "For In", check the next module
+}
---
+++ Course/Intermediate/4_difference_for_in_and_for_each.dart
@@ -0,0 +1,44 @@
+void main() {
+  // Difference between "For In" and "For Each" loop.
+
+  print('\n\n Difference: \n');
+  var a = 'A';
+  var b = 'B';
+  var c = 'C';
+
+  var array_b = [a, b, c];
+
+  for (var item in array_b) {
+    print(item);
+  }
+
+  array_b.forEach((element) {
+    print(element);
+  });
+
+  // Difference 2
+
+  print('\n\n\n No Difference found. phase 2: \n\n');
+
+  var x = 'x';
+  var y = 'y';
+  var z = 'z';
+
+  dynamic obj_container = {x: 'X', y: 'Y', z: 'Z'}; // It's data type is "Map"
+
+  obj_container.forEach((key, value) {
+    print('$key : $value');
+  });
+
+  // This will raise exception
+  for (var item in obj_container) {
+    print('$item');
+  }
+
+  // 'For In' can be used with lists only and not with maps
+  /*
+  'For Each' can be used with both lists and maps but, 
+  Map items can have properties, While list items cannot have properties.
+  Map item's properties can also be accessed from For in loop.  
+  */
+}
