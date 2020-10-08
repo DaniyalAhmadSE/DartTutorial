b7ffbba760107c9093e09d2507174e59d08b16c0
Daniyal Ahmad <57293258+Daniyal-Ahmad358@users.noreply.github.com>
2020-10-09 00:11:17 +0500

    Classes and their constructors.

---
+++ Course/Intermediate/2u_Classes.dart
@@ -0,0 +1,20 @@
+class Point {
+  // Data Members
+  int x;
+  int y;
+
+  // Constructor:
+  /*
+  > The name of a constructor is same as that of the class,
+  > Inside the constructor we initialize the defined variables.
+  */
+  Point() {
+    x = 0;
+    y = 0;
+  }
+}
+
+void main() {
+  // Point p;
+  // p = Point();
+}
