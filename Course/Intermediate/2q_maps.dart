3980f796999f24146f50a0f72c309c6a49b2f94f
Daniyal Ahmad <57293258+Daniyal-Ahmad358@users.noreply.github.com>
2020-10-08 16:36:10 +0500

    Map collections in Dart

---
+++ Course/Intermediate/2q_maps.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Maps
+
+  Map student_details;
+  student_details = {
+    'name': 'Daniyal',
+    'age': 18,
+    'email': 'daniyalahmad358@gmail.com'
+  };
+
+  print(student_details);
+
+  // Printing the value of a specipic 'Key' from a map.
+  print(student_details['name']);
+}
