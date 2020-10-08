d87a69c2315f2132db5160667089fad43dffe2a4
Daniyal Ahmad <57293258+Daniyal-Ahmad358@users.noreply.github.com>
2020-10-08 16:35:04 +0500

    Fixed convention issue.

--- Course/Intermediate/2n_list.dart
+++ Course/Intermediate/2n_list.dart
@@ -4,7 +4,6 @@ void main() {
     > Lists are similar to arrays.
     > It is also a data type in dart.
   */
-
   List a_list;
   a_list = [1, 2, 3];
   print(a_list);
@@ -20,15 +19,12 @@ void main() {
     So it will raise exception.
   > We can change a value with another value of a different data type.
   */
-
   b_list[1] = 'rat';
-
   print(b_list);
 
   // Limiting string data type:
   List<String> c_list;
   c_list = ['apple', 'book', 'cat'];
-
   /*
     Changing data type of values i.e. c_list[2] = 1; ,
     will raise error, as I have limited the List to only contain string values.
@@ -36,12 +32,11 @@ void main() {
   print(c_list);
 
   // Making list items unchangable
-
   List d_list;
   d_list = const ['x', 'y', 'z'];
-
   /*
     Changing list values i.e. d_list[0] = 'P'; ,
     will raise error, as I have set the List items as constant values.
   */
+  print(d_list);
 }
